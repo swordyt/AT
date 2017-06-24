f6f10f581f9a309b65853cd112d0cfeaa7ab684b
YinTing <swordyt@163.com>
2017-06-25 02:30:47 +0800

    dbDriver

---
+++ AutoTest/src/main/java/com/yinting/dao/Db.java
@@ -0,0 +1,7 @@
+package com.yinting.dao;
+
+import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
+
+public class Db extends SqlMapClientDaoSupport{
+
+}
---
+++ AutoTest/src/main/java/com/yinting/dao/User.java
@@ -0,0 +1,221 @@
+package com.yinting.dao;
+
+public class User {
+
+	private String iAutoID;
+	private String sUserName;
+	private String sNickname;
+	private String sRealname;
+	private String iGender;
+	private String sHeadPortraitKey;
+	private String sHeadPortraitExt;
+	private String sHgjJzToken;
+	private String sMobile;
+	private String sPassword;
+	private String sPayPassword;
+	private String iStatus;
+	private String iCreateTime;
+	private String iUpdateTime;
+	private String iVerifyMobile;
+	private String iLevel;
+	private String iDSType;
+	private String iBroker;
+	private String iUsableIntegral;
+	private String iHistoryIntegral;
+	private String sDigitalPayPassword;
+	private String iLock;
+	private String iLockEndTime;
+	private String sDoorLockPassword;
+
+	public String getiAutoID() {
+		return iAutoID;
+	}
+
+	public void setiAutoID(String iAutoID) {
+		this.iAutoID = iAutoID;
+	}
+
+	public String getsUserName() {
+		return sUserName;
+	}
+
+	public void setsUserName(String sUserName) {
+		this.sUserName = sUserName;
+	}
+
+	public String getsNickname() {
+		return sNickname;
+	}
+
+	public void setsNickname(String sNickname) {
+		this.sNickname = sNickname;
+	}
+
+	public String getsRealname() {
+		return sRealname;
+	}
+
+	public void setsRealname(String sRealname) {
+		this.sRealname = sRealname;
+	}
+
+	public String getiGender() {
+		return iGender;
+	}
+
+	public void setiGender(String iGender) {
+		this.iGender = iGender;
+	}
+
+	public String getsHeadPortraitKey() {
+		return sHeadPortraitKey;
+	}
+
+	public void setsHeadPortraitKey(String sHeadPortraitKey) {
+		this.sHeadPortraitKey = sHeadPortraitKey;
+	}
+
+	public String getsHeadPortraitExt() {
+		return sHeadPortraitExt;
+	}
+
+	public void setsHeadPortraitExt(String sHeadPortraitExt) {
+		this.sHeadPortraitExt = sHeadPortraitExt;
+	}
+
+	public String getsHgjJzToken() {
+		return sHgjJzToken;
+	}
+
+	public void setsHgjJzToken(String sHgjJzToken) {
+		this.sHgjJzToken = sHgjJzToken;
+	}
+
+	public String getsMobile() {
+		return sMobile;
+	}
+
+	public void setsMobile(String sMobile) {
+		this.sMobile = sMobile;
+	}
+
+	public String getsPassword() {
+		return sPassword;
+	}
+
+	public void setsPassword(String sPassword) {
+		this.sPassword = sPassword;
+	}
+
+	public String getsPayPassword() {
+		return sPayPassword;
+	}
+
+	public void setsPayPassword(String sPayPassword) {
+		this.sPayPassword = sPayPassword;
+	}
+
+	public String getiStatus() {
+		return iStatus;
+	}
+
+	public void setiStatus(String iStatus) {
+		this.iStatus = iStatus;
+	}
+
+	public String getiCreateTime() {
+		return iCreateTime;
+	}
+
+	public void setiCreateTime(String iCreateTime) {
+		this.iCreateTime = iCreateTime;
+	}
+
+	public String getiUpdateTime() {
+		return iUpdateTime;
+	}
+
+	public void setiUpdateTime(String iUpdateTime) {
+		this.iUpdateTime = iUpdateTime;
+	}
+
+	public String getiVerifyMobile() {
+		return iVerifyMobile;
+	}
+
+	public void setiVerifyMobile(String iVerifyMobile) {
+		this.iVerifyMobile = iVerifyMobile;
+	}
+
+	public String getiLevel() {
+		return iLevel;
+	}
+
+	public void setiLevel(String iLevel) {
+		this.iLevel = iLevel;
+	}
+
+	public String getiDSType() {
+		return iDSType;
+	}
+
+	public void setiDSType(String iDSType) {
+		this.iDSType = iDSType;
+	}
+
+	public String getiBroker() {
+		return iBroker;
+	}
+
+	public void setiBroker(String iBroker) {
+		this.iBroker = iBroker;
+	}
+
+	public String getiUsableIntegral() {
+		return iUsableIntegral;
+	}
+
+	public void setiUsableIntegral(String iUsableIntegral) {
+		this.iUsableIntegral = iUsableIntegral;
+	}
+
+	public String getiHistoryIntegral() {
+		return iHistoryIntegral;
+	}
+
+	public void setiHistoryIntegral(String iHistoryIntegral) {
+		this.iHistoryIntegral = iHistoryIntegral;
+	}
+
+	public String getsDigitalPayPassword() {
+		return sDigitalPayPassword;
+	}
+
+	public void setsDigitalPayPassword(String sDigitalPayPassword) {
+		this.sDigitalPayPassword = sDigitalPayPassword;
+	}
+
+	public String getiLock() {
+		return iLock;
+	}
+
+	public void setiLock(String iLock) {
+		this.iLock = iLock;
+	}
+
+	public String getiLockEndTime() {
+		return iLockEndTime;
+	}
+
+	public void setiLockEndTime(String iLockEndTime) {
+		this.iLockEndTime = iLockEndTime;
+	}
+
+	public String getsDoorLockPassword() {
+		return sDoorLockPassword;
+	}
+
+	public void setsDoorLockPassword(String sDoorLockPassword) {
+		this.sDoorLockPassword = sDoorLockPassword;
+	}
+}
